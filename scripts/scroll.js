adf28ee0e5cd51f8b8d4996b5a0a8981051e13f6
Dmytro <70340297+Dmytrosat@users.noreply.github.com>
2022-01-22 17:21:51 +0200

    Add files via upload

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,33 +1,29 @@
 const scrollFunc = () => {
-    const links = document.querySelectorAll('.header-menu__item a');
-    const linkCharacteristics = document.querySelectorAll('.card-details__link-characteristics');
-    
-    const newArray = [...links, linkCharacteristics]
-    
-    seamless.polyfill();
-    
-    newArray.forEach((element) => {
-        element.addEventListener('click', (event) => {
+    const links = document.querySelectorAll('.header-menu__item a')
+    const details = document.querySelector('.card-details__link-characteristics')
+    const arrayWithLinks = [...links, details]
+
+    seamless.polyfill()
+
+    arrayWithLinks.forEach((link) => {
+        link.addEventListener('click', event => {
             event.preventDefault()
-    
-            const id = element.getAttribute('href').substring(1)
+
+            const id = link.getAttribute('href').substring(1)
             const section = document.getElementById(id)
-            
             if (section) {
                 seamless.elementScrollIntoView(section, {
                     behavior: 'smooth',
                     block: 'start'
-                });
+                })
             } else {
                 seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
                     behavior: "smooth",
-                    block: "center",
-                    inline: "center",
-                });
+                    block: "start",
+                })
             }
         })
     })
-    
 }
 
 scrollFunc()
