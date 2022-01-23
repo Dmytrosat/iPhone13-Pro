4a782b34ee3d64ea94469b3707f4dab04b161db1
Dmytro <70340297+Dmytrosat@users.noreply.github.com>
2022-01-23 16:30:43 +0200

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,29 @@
+const scrollFunc = () => {
+    const links = document.querySelectorAll('.header-menu__item a')
+    const details = document.querySelector('.card-details__link-characteristics')
+    const arrayWithLinks = [...links, details]
+
+    seamless.polyfill()
+
+    arrayWithLinks.forEach((link) => {
+        link.addEventListener('click', event => {
+            event.preventDefault()
+
+            const id = link.getAttribute('href').substring(1)
+            const section = document.getElementById(id)
+            if (section) {
+                seamless.elementScrollIntoView(section, {
+                    behavior: 'smooth',
+                    block: 'start'
+                })
+            } else {
+                seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
+                    behavior: "smooth",
+                    block: "start",
+                })
+            }
+        })
+    })
+}
+
+scrollFunc()
